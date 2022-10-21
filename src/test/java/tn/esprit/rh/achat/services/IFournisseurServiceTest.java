58e63f87f8f10e4867b4a483e5671ba7a01f417e
ahmedaziz-code <ahmedaziz.elj@esprit.tn>
2022-10-21 13:21:46 +0100

    add founisseur test

---
+++ src/test/java/tn/esprit/rh/achat/services/IFournisseurServiceTest.java
@@ -0,0 +1,37 @@
+package tn.esprit.rh.achat.services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class IFournisseurServiceTest {
+    @Autowired
+    IFournisseurService fournisseurService;
+
+    @Test
+    void retrieveAllFournisseurs() {
+    }
+
+    @Test
+    void addFournisseur() {
+    }
+
+    @Test
+    void deleteFournisseur() {
+    }
+
+    @Test
+    void updateFournisseur() {
+    }
+
+    @Test
+    void retrieveFournisseur() {
+    }
+
+    @Test
+    void assignSecteurActiviteToFournisseur() {
+    }
+}
