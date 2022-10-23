b29fd8fc3379762272cfc9eb33482e6d0204377b
ahmedaziz-code <ahmedaziz.elj@esprit.tn>
2022-10-23 19:08:07 +0100

    update fournisseur test

--- src/test/java/tn/esprit/rh/achat/services/IFournisseurServiceTest.java
+++ src/test/java/tn/esprit/rh/achat/services/IFournisseurServiceTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+//@SpringBootTest
 class IFournisseurServiceTest {
     @Autowired
     IFournisseurService fournisseurService;
