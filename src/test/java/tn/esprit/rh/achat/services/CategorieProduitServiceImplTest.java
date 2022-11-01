63be726b28cfbcee3ef5930556993ee8e90c8257
lotfi2020-2021 <lotfi.hmida@esprit.tn>
2022-11-01 19:40:11 +0100

    test1

---
+++ src/test/java/tn/esprit/rh/achat/services/CategorieProduitServiceImplTest.java
@@ -0,0 +1,34 @@
+package tn.esprit.rh.achat.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CategorieProduitServiceImplTest {
+
+	@Test
+	void testRetrieveAllCategorieProduits() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetrieveCategorieProduit() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/test/java/tn/esprit/rh/achat/services/ProduitServiceImplTest.java
@@ -0,0 +1,39 @@
+package tn.esprit.rh.achat.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProduitServiceImplTest {
+
+	@Test
+	void testRetrieveAllProduits() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRetrieveProduit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAssignProduitToStock() {
+		fail("Not yet implemented");
+	}
+
+}
