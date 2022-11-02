ea0f2f0abc2e34bd9726b81e1d1ae562452ecd53
Ahmed-ZARRAI <ahmed.zarrai@esprit.tn>
2022-11-02 14:32:49 +0100

    retiree des debugs ReglementServiceImpl

--- src/test/java/tn/esprit/rh/achat/services/ReglementServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/ReglementServiceImplTest.java
@@ -1,4 +1,28 @@
+package tn.esprit.rh.achat.services;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ReglementServiceImplTest {
-  
+
+    @Test
+    void retrieveAllReglements() {
+    }
+
+    @Test
+    void addReglement() {
+    }
+
+    @Test
+    void retrieveReglement() {
+    }
+
+    @Test
+    void retrieveReglementByFacture() {
+    }
+
+    @Test
+    void getChiffreAffaireEntreDeuxDate() {
+    }
 }
