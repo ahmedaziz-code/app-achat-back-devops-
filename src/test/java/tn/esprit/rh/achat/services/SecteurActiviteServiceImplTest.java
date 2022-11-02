a085d7215ce20f375522314a0961c965fb62249e
Ahmed-ZARRAI <ahmed.zarrai@esprit.tn>
2022-11-02 14:33:05 +0100

    retiree des debugs ReglementServiceImpl

--- src/test/java/tn/esprit/rh/achat/services/SecteurActiviteServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/SecteurActiviteServiceImplTest.java
@@ -1,4 +1,28 @@
+package tn.esprit.rh.achat.services;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SecteurActiviteServiceImplTest {
-  
+
+    @Test
+    void retrieveAllSecteurActivite() {
+    }
+
+    @Test
+    void addSecteurActivite() {
+    }
+
+    @Test
+    void deleteSecteurActivite() {
+    }
+
+    @Test
+    void updateSecteurActivite() {
+    }
+
+    @Test
+    void retrieveSecteurActivite() {
+    }
 }
