2bc925358d5fb9e159495b6e87e2f921e2cfb386
ahmedaziz-code <ahmedaziz.elj@esprit.tn>
2022-11-03 14:10:37 +0100

    test facture controller

---
+++ src/test/java/tn/esprit/rh/achat/controllers/FactureRestControllerTest.java
@@ -0,0 +1,71 @@
+package tn.esprit.rh.achat.controllers;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.rh.achat.entities.Facture;
+import tn.esprit.rh.achat.services.FactureServiceImpl;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class FactureRestControllerTest {
+    @Mock
+    FactureServiceImpl factureService;
+    @InjectMocks
+    FactureRestController factureRestController;
+
+    Facture facture = new Facture((float) 7.4, (float) 99.2,new Date(),new Date(),false);
+
+    List<Facture> listFacture = new ArrayList<Facture>(){
+        {
+            add(new Facture((float) 6.4, (float) 99.2,new Date(),new Date(),true));
+            add(new Facture((float) 7.9, (float) 99.2,new Date(),new Date(),true));
+        }
+    };
+
+    @Test
+    @Order(1)
+    void getFactures() {
+        Mockito.when(factureService.retrieveFacture(Mockito.anyLong())).thenReturn(facture);
+        Facture savedFacture = factureRestController.retrieveFacture(2L);
+        assertNotNull(savedFacture);
+    }
+
+    @Test
+    void retrieveFacture() {
+    }
+
+    @Test
+    void addFacture() {
+    }
+
+    @Test
+    void cancelFacture() {
+    }
+
+    @Test
+    void getFactureByFournisseur() {
+    }
+
+    @Test
+    void assignOperateurToFacture() {
+    }
+
+    @Test
+    void pourcentageRecouvrement() {
+    }
+}
