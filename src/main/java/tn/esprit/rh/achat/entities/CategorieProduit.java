eb7b99a7bd7e89a25ae1d502308abf988a08c12a
lotfi2020-2021 <lotfi.hmida@esprit.tn>
2022-11-03 12:46:08 +0100

    debug categorieProduit

--- src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
+++ src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
@@ -32,4 +32,12 @@ public class CategorieProduit implements Serializable {
 	@OneToMany(mappedBy = "categorieProduit")
 	@JsonIgnore
 	private Set<Produit> produits;
+	
+	public CategorieProduit(String codeCategorie, String libelleCategorie) {
+		super();
+		this.codeCategorie = codeCategorie;
+		this.libelleCategorie = libelleCategorie;
+	}
+	
+	
 }
