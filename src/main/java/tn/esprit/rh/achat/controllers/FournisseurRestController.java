70ab04c8deae0aa6b1b91be25ed39faab6191067
ahmedaziz-code <ahmedaziz.elj@esprit.tn>
2022-11-08 15:21:29 +0100

    cross origins 80

--- src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/FournisseurRestController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @Api(tags = "Gestion des fournisseurss")
 @RequestMapping("/fournisseur")
-@CrossOrigin("*")
+@CrossOrigin("80")
 public class FournisseurRestController {
 
 	@Autowired
