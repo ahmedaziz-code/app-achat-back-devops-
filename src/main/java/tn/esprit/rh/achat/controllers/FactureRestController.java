e68abe5bbd16bba79f3c11e57e33cab03f5b412f
ahmedaziz-code <ahmedaziz.elj@esprit.tn>
2022-11-08 17:02:24 +0100

    cross origin *

--- src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
+++ src/main/java/tn/esprit/rh/achat/controllers/FactureRestController.java
@@ -16,7 +16,7 @@ import java.util.List;
 @RestController
 @Api(tags = "Gestion des factures")
 @RequestMapping("/facture")
-@CrossOrigin("80")
+@CrossOrigin("*")
 public class FactureRestController {
 
 
